8d27162b3f260b7ee6f1af768f1787038648d791
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-08-23 17:42:09 +0700

    Update worker-pool.js

--- src/worker-pool.js
+++ src/worker-pool.js
@@ -1,31 +1,93 @@
-const { 
-  hashBlock, 
-  verifySignature, 
-  canonicalTxMessage,
-  ZERO_HASH 
-} = require('./crypto');
+const { Worker } = require('worker_threads');
+const path = require('path');
+
+// Đường dẫn tuyệt đối đến crypto.js (cùng thư mục)
+const cryptoPath = path.join(__dirname, 'crypto.js');
+
+// Mã nguồn worker (CommonJS) sẽ chạy trong thread riêng
+const workerCode = `
+  const { parentPort } = require('worker_threads');
+  const { hashBlock, verifySignature, canonicalTxMessage } = require(${JSON.stringify(cryptoPath)});
+
+  parentPort.on('message', (msg) => {
+    let result;
+    try {
+      switch (msg.type) {
+        case 'hashBlock': result = hashBlock(msg.block); break;
+        case 'verifySignature': result = verifySignature(msg.message, msg.signature, msg.pubkey); break;
+        case 'canonicalTxMessage': result = canonicalTxMessage(msg.tx); break;
+        default: result = undefined;
+      }
+      parentPort.postMessage({ id: msg.id, result });
+    } catch (error) {
+      parentPort.postMessage({ id: msg.id, error: error.message });
+    }
+  });
+`;
+
+let worker = null;
+let nextId = 0;
+const pending = new Map();
+
+function getWorker() {
+  if (!worker) {
+    worker = new Worker(workerCode, { eval: true });
+    worker.on('message', (data) => {
+      const p = pending.get(data.id);
+      if (p) {
+        pending.delete(data.id);
+        if (data.error) p.reject(new Error(data.error));
+        else p.resolve(data.result);
+      }
+    });
+    worker.on('error', (err) => {
+      for (const p of pending.values()) p.reject(err);
+      pending.clear();
+      worker = null;
+    });
+    worker.on('exit', () => {
+      for (const p of pending.values()) p.reject(new Error('Worker exited'));
+      pending.clear();
+      worker = null;
+    });
+  }
+  return worker;
+}
+
+function schedule(type, payload) {
+  const id = nextId++;
+  return new Promise((resolve, reject) => {
+    pending.set(id, { resolve, reject });
+    getWorker().postMessage({ id, type, ...payload });
+  });
+}
 
 async function hashBlockAsync(block) {
-  return hashBlock(block);
+  return schedule('hashBlock', { block });
 }
 
 async function verifySignatureAsync(message, signature, pubkey) {
-  return verifySignature(message, signature, pubkey);
+  return schedule('verifySignature', { message, signature, pubkey });
 }
 
 async function canonicalTxMessageAsync(tx) {
-  return canonicalTxMessage(tx);
+  return schedule('canonicalTxMessage', { tx });
 }
 
 async function hashBlocksAsync(blocks) {
-  return blocks.map(hashBlock);
+  return Promise.all(blocks.map(b => hashBlockAsync(b)));
 }
 
 async function verifySignaturesAsync(items) {
-  return items.map(i => verifySignature(i.message, i.signature, i.pubkey));
+  return Promise.all(items.map(i => verifySignatureAsync(i.message, i.signature, i.pubkey)));
 }
 
-function terminateWorkerPool() {}
+function terminateWorkerPool() {
+  if (worker) {
+    worker.terminate();
+    worker = null;
+  }
+}
 
 module.exports = {
   hashBlockAsync,
